288c1a9d10d9f2823a6312274538dec34d2400a3
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-04-19 23:15:05 -0400

    edit button and methods that allows delete the row has been set

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -10,6 +10,10 @@ class MealTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // this creates "edit" button with built-in behavior
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        // load the sample data
         loadSampleMeals()
     }
 
@@ -44,25 +48,27 @@ class MealTableViewController: UITableViewController {
     }
     
 
-    /*
+//
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
+    // tableView's delegate method - manages changes to the table row when it's in editing mode
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            // deleting the meal from the data source
+            meals.remove(at: indexPath.row)
+            // this removes the row from table view
             tableView.deleteRows(at: [indexPath], with: .fade)
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
