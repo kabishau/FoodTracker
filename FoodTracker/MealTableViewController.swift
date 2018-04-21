9007c3f44b7eeca5201c810bbde7e9d04aca6b3f
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-04-21 13:28:26 -0400

    implementing the method to save the meal list

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -166,5 +166,27 @@ class MealTableViewController: UITableViewController {
         
         meals += [meal1, meal2, meal3]
     }
+    
+    private func saveMeals() {
+        
+        // method attempts to archive the meals to a specific location and return true if it's successfull
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+        
+        if isSuccessfulSave {
+            os_log("Meals successfully saved", log: .default, type: .debug)
+        } else {
+            os_log("Failed to save meals...", log: .default, type: .debug)
+        }
+        
+    }
 
 }
+
+
+
+
+
+
+
+
+
